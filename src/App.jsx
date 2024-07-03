4ca806186112ff6dd9469675713871bbfbca4fad
pradeepasri26 <pradeepasri2003@gmail.com>
2024-07-03 14:17:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+import TodoList from './TodoList';
+
+const App = () => {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+};
+
+export default App;
